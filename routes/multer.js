94a400b7789ad4e6555ac6046cf70eaa756d6ad8
ankita239 <ankitaacharya.aa.01@gmail.com>
2024-07-10 22:31:42 +0530

    Use connect-mongo for session management

--- routes/multer.js
+++ routes/multer.js
@@ -6,7 +6,7 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 // Make sure to set these environment variables in your hosting environment
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
